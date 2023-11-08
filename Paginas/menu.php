fa40651064ed1296ca0d48e485abf3e68b8477c8
Ronald Henrick <ronaldhenrick@gmail.com>
2023-11-08 15:12:04 -0300

    .

--- Paginas/menu.php
+++ Paginas/menu.php
@@ -1,6 +1,6 @@
 <link rel="stylesheet" href="menu.css">
 <h1 class="menu-color">Menu</h1>
-<div class="menu"
+<div class="menu">
   <a href="?pg=quemSomos">Quem Somos</a><br>
   <a href="?pg=cursos">Cursos</a><br>
   <a href="?pg=faleConosco">Fale Conosco</a>
