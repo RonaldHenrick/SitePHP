3ea79bf876224b5617877e6405ac9640f2ba5391
Vinicius Montenegro <130661429+viniciusmontenegro04@users.noreply.github.com>
2023-11-21 15:48:45 -0300

    Css Admin

--- admin/index.php
+++ admin/index.php
@@ -12,19 +12,20 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="description" content="">
     <meta name="author" content="">
-
+    <link rel="stylesheet" href="index.css">
     <title>DevMentor.com - Admin</title>
 </head>
 <body>
-                <h1>DevMentor - Admin </h1>
+            <div class="admin-table">
+                <h1>DevMentor - Área do Administrador </h1>
                 <ol>
-                    <li><a href="../Paginas/index.php">Home</a>
-                    </li>
+                    <li><a href="../Paginas/index.php">Home</a></li>
                     <li><a href="listarUsuarios.php">Listar Usuários</a></li>
                     <li><a href="post.php">Inserir Nova Página</a></li>
                     <li><a href="listarPost.php">Excluir Post</a></li>
                     <li><a href="atualizar.php">Atualizar Post</a></li>
                 </ol>
+            </div>
             </div>
         </div>
 
